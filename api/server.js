f0ace3dd47a950ef45deb7c1aa5ac2335e5a74b6
Vyraal1 <kevin.afable@gmail.com>
2019-11-18 17:29:51 -0500

    FEAT added server setup

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../users/user-router");
+// const analysisRouter = require("../analysis/analysis-router");
+
+const configureMiddleware = require("./configure-middleware");
+
+const server = express();
+
+//middleware configurator for remote modules
+configureMiddleware(server);
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+// server.use("/api/analysis", analysisRouter);
+
+module.exports = server;
