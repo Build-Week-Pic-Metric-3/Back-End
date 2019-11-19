710e44487d63370e8c43e89649e8cc4066478880
Vyraal1 <kevin.afable@gmail.com>
2019-11-18 23:45:01 -0500

    FEAT: Analysis table

--- database/migrations/20191118231617_analysis.js
+++ database/migrations/20191118231617_analysis.js
@@ -1,22 +1,28 @@
 exports.up = function(knex) {
-  return knex.schema.createTable(analysis => {
+  return knex.schema.createTable("analysis", analysis => {
     analysis.increments();
+
+    //foreign only works on a pre-existing column
+    analysis.integer("user_id");
     analysis
       .foreign("user_id")
-      .references("user.id")
+      .references("users.id")
       .onUpdate("cascade")
       .onDelete("cascade");
+
+    analysis.integer("photo_id");
     analysis
       .foreign("photo_id")
-      .references("photo.id")
+      .references("photos.id")
       .onUpdate("cascade")
       .onDelete("cascade");
+
     analysis.varchar("pred1", 255);
-    analysis.varchar("pred1_val", 255);
+    analysis.float("pred1_val");
     analysis.varchar("pred2", 255);
-    analysis.varchar("pred2_val", 255);
+    analysis.float("pred2_val");
     analysis.varchar("pred3", 255);
-    analysis.varchar("pred3_val", 255);
+    analysis.float("pred3_val");
   });
 };
 
