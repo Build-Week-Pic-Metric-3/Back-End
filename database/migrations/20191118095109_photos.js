c859ceb86433130f08addfbb6a5c4cd4c3aeea90
Vyraal1 <kevin.afable@gmail.com>
2019-11-18 23:27:13 -0500

    CHORE: formatting migrations

--- database/migrations/20191118095109_photos.js
+++ database/migrations/20191118095109_photos.js
@@ -1,13 +1,11 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("images", images => {
-    images.increments();
-
-    images.varchar("hash", 255).notNullable();
-
-    images.varchar("source", 255).notNullable();
+  return knex.schema.createTable("photos", photos => {
+    photos.increments();
+    photos.varchar("hash", 255).notNullable();
+    photos.varchar("source", 255).notNullable();
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("photos");
 };
