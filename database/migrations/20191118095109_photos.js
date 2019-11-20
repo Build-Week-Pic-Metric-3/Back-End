3cf67c66b2e9696b47f42f4c69b360f0deed509c
Vyraal1 <kevin.afable@gmail.com>
2019-11-20 17:14:43 -0500

    FEAT Moved to only two tables

--- database/migrations/20191118095109_photos.js
+++ database/migrations/20191118095109_photos.js
@@ -1,11 +1,21 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("images", photos => {
-    photos.increments();
-    photos.varchar("hash", 255).notNullable();
-    photos.varchar("source", 255).notNullable();
+  return knex.schema.createTable("analysis", analysis => {
+    analysis.increments();
+    analysis.varchar("hash", 255).notNullable();
+    analysis.varchar("source", 255).notNullable();
+
+    analysis.integer("user_id");
+    analysis
+      .foreign("user_id")
+      .references("users.id")
+      .onUpdate("cascade")
+      .onDelete("cascade");
+
+    analysis.string("resnet");
+    analysis.string("yolov3");
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("images");
+  return knex.schema.dropTableIfExists("analysis");
 };
